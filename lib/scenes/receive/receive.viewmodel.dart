aa84a3e7bdf82d6f33a84eea659165ace53aa953
yzhang <yanfeng.zhang@proton.ch>
2023-12-11 17:45:15 +0800

    update receive view

--- lib/scenes/receive/receive.viewmodel.dart
+++ lib/scenes/receive/receive.viewmodel.dart
@@ -1,12 +1,20 @@
 import 'dart:async';
+import 'package:wallet/helper/bdk/helper.dart';
 import 'package:wallet/scenes/core/viewmodel.dart';
+import 'package:wallet/scenes/debug/bdk.test.dart';
 
 abstract class ReceiveViewModel extends ViewModel {
   ReceiveViewModel(super.coordinator);
+
+  String address = "";
 }
 
 class ReceiveViewModelImpl extends ReceiveViewModel {
   ReceiveViewModelImpl(super.coordinator);
+
+  final BdkLibrary _lib = BdkLibrary();
+  late Wallet _wallet;
+  // Blockchain? blockchain;
   final datasourceChangedStreamController =
       StreamController<ReceiveViewModel>.broadcast();
   @override
@@ -15,9 +23,25 @@ class ReceiveViewModelImpl extends ReceiveViewModel {
   }
 
   @override
-  Future<void> loadData() async {}
+  Future<void> loadData() async {
+    //restore wallet
+    final aliceMnemonic = await Mnemonic.fromString(
+        'certain sense kiss guide crumble hint transfer crime much stereo warm coral');
+    final aliceDescriptor = await _lib.createDescriptor(aliceMnemonic);
+    _wallet = await _lib.restoreWallet(aliceDescriptor);
+    // blockchain ??= await _lib.initializeBlockchain(false);
+    getAddress();
+  }
 
   @override
   Stream<ViewModel> get datasourceChanged =>
       datasourceChangedStreamController.stream;
+
+  void getAddress() async {
+    var addressinfo = await _lib.getAddress(_wallet);
+
+    address = addressinfo.address;
+
+    datasourceChangedStreamController.add(this);
+  }
 }
