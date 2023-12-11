490b679d8e600a5c53f0e6066de151bed388d7ec
Will Hsu <will.hsu@proton.ch>
2023-12-11 17:01:24 +0800

    Moving files from ProtonPass repo

---
+++ android/app/src/main/kotlin/com/example/wallet/log/api/WalletLogger.kt
@@ -0,0 +1,6 @@
+package com.example.wallet.log.api
+
+import me.proton.core.util.android.sentry.TimberLogger
+import me.proton.core.util.kotlin.Logger
+
+object WalletLogger : Logger by TimberLogger
