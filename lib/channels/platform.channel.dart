bc798af5db095c0d6bf0ae177f06561817b44a54
yzhang <yanfeng.zhang@proton.ch>
2024-04-15 23:19:04 +0800

    Add env switch. control api url in flutter code

--- lib/channels/platform.channel.dart
+++ lib/channels/platform.channel.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/services.dart';
+import 'package:wallet/constants/env.dart';
 import 'package:wallet/helper/logger.dart';
 
 class NativeViewSwitcher {
   static const platform = MethodChannel('com.example.wallet/native.views');
+  static const _envKey = "env-key";
 
-  static Future<void> switchToNativeSignup() async {
+  static Future<void> switchToNativeSignup(ApiEnv env) async {
     try {
-      await platform.invokeMethod('native.navigation.signup');
+      await platform
+          .invokeMethod('native.navigation.signup', {_envKey: env.toString()});
     } on PlatformException catch (e) {
       logger.e("Failed to switch to native view: '${e.message}'.");
     }
   }
 
-  static Future<void> switchToNativeLogin() async {
+  static Future<void> switchToNativeLogin(ApiEnv env) async {
     try {
-      await platform.invokeMethod('native.navigation.login');
+      await platform
+          .invokeMethod('native.navigation.login', {_envKey: env.toString()});
     } on PlatformException catch (e) {
       logger.e("Failed to switch to native view: '${e.message}'.");
     }
