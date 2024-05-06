543287db70d226c208e5584bb5d4a0879ace469d
Will Hsu <will.hsu@proton.ch>
2024-05-06 15:23:13 +0800

    set android to use prod

--- android/app/src/main/kotlin/me/proton/core/configuration/EnvironmentConfigurationDefaults.java
+++ android/app/src/main/kotlin/me/proton/core/configuration/EnvironmentConfigurationDefaults.java
@@ -1,12 +1,12 @@
 package me.proton.core.configuration;
 
 public class EnvironmentConfigurationDefaults {
-    public static final String host = "pascal.proton.black";
+    public static final String host = "proton.me";
     public static final String proxyToken = "";
-    public static final String apiPrefix = "api";
-    public static final String baseUrl = "https://api.pascal.proton.black";
-    public static final String apiHost = "api.pascal.proton.black";
-    public static final String hv3Host = "verify.pascal.proton.black";
-    public static final String hv3Url = "https://verify.pascal.proton.black";
+    public static final String apiPrefix = "wallet-api";
+    public static final String baseUrl = "https://wallet-api.proton.me";
+    public static final String apiHost = "wallet-api.proton.me";
+    public static final String hv3Host = "verify.proton.me";
+    public static final String hv3Url = "https://verify.proton.me";
     public static final Boolean useDefaultPins = false;
 }
