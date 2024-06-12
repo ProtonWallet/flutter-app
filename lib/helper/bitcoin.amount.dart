e3eec75338448da50aec79b2ab07777eeed3e104
Will Hsu <will.hsu@proton.ch>
2024-06-12 14:56:06 +0800

    refactor UI

---
+++ lib/helper/bitcoin.amount.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+import 'package:wallet/helper/exchange.caculator.dart';
+import 'package:wallet/rust/proton_api/exchange_rate.dart';
+import 'package:wallet/rust/proton_api/user_settings.dart';
+
+class BitcoinAmount {
+  int amountInSatoshi;
+  BitcoinUnit bitcoinUnit;
+  ProtonExchangeRate exchangeRate;
+
+  BitcoinAmount({
+    required this.amountInSatoshi,
+    required this.bitcoinUnit,
+    required this.exchangeRate,
+  });
+
+  double getNotionalInFiatCurrency() {
+    return ExchangeCalculator.getNotionalInFiatCurrency(
+        exchangeRate, amountInSatoshi);
+  }
+
+  int getDisplayDigit() {
+    return (log(exchangeRate.cents) / log(10)).round();
+  }
+
+  String toFiatCurrencyString() {
+    int displayDigits = getDisplayDigit();
+    double amountInFiatCurrency = getNotionalInFiatCurrency().abs();
+    String amountString = amountInFiatCurrency.toStringAsFixed(displayDigits);
+    String fiatCurrencyName =
+        exchangeRate.fiatCurrency.name.toString().toUpperCase();
+    if (amountInSatoshi > 0) {
+      return "$fiatCurrencyName $amountString";
+    }
+    return "-$fiatCurrencyName $amountString";
+  }
+
+  @override
+  String toString() {
+    return ExchangeCalculator.getBitcoinUnitLabel(bitcoinUnit, amountInSatoshi);
+  }
+}
