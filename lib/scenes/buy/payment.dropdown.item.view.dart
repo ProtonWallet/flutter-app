c5e56347cf5ac528ad2d1c5a2c73df49a1cc4e2c
yzhang <yanfeng.zhang@proton.ch>
2024-07-12 00:56:58 +0800

    update ramp

--- lib/scenes/buy/payment.dropdown.item.view.dart
+++ lib/scenes/buy/payment.dropdown.item.view.dart
@@ -1,72 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:wallet/constants/proton.color.dart';
 import 'package:wallet/scenes/buy/payment.dropdown.item.dart';
 
 class PaymentDropdownItem extends StatelessWidget {
   final DropdownItem item;
+  final Widget icon;
+  final bool selected;
   final VoidCallback? onTap;
   const PaymentDropdownItem({
     super.key,
     required this.item,
+    required this.icon,
+    this.selected = false,
     this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        /// payment icon
-        Container(
-          width: 40,
-          height: 40,
-          decoration: ShapeDecoration(
-            color: const Color(0xFFF3F5F6),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(200),
-            ),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.only(left: 8, top: 8, right: 8, bottom: 8),
+        decoration: ShapeDecoration(
+          color: selected ? ProtonColors.textWeakPressed : null,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 16,
-                height: 16,
-                clipBehavior: Clip.antiAlias,
-                decoration: const BoxDecoration(),
-                child: const FlutterLogo(),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            /// payment icon
+            Container(
+              width: 40,
+              height: 40,
+              decoration: ShapeDecoration(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(200),
+                ),
               ),
-            ],
-          ),
-        ),
-        const SizedBox(width: 16),
-        Expanded(
-          child: Text(
-            // 'Credit Card',
-            item.title,
-            style: const TextStyle(
-              color: Color(0xFF0C0C14),
-              fontSize: 14,
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w500,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 16,
+                    height: 16,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: const BoxDecoration(),
+                    child: icon,
+                  ),
+                ],
+              ),
             ),
-          ),
+            const SizedBox(width: 16),
+            Text(
+              // 'Credit Card',
+              item.title,
+              style: const TextStyle(
+                color: Color(0xFF0C0C14),
+                fontSize: 14,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Expanded(
+              child: Text(
+                item.subtitle,
+                textAlign: TextAlign.right,
+                style: const TextStyle(
+                  color: Color(0xFF191C32),
+                  fontSize: 14,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ],
         ),
-        Text(
-          item.subtitle,
-          // 'Take minutes',
-          textAlign: TextAlign.right,
-          style: const TextStyle(
-            color: Color(0xFF191C32),
-            fontSize: 14,
-            fontFamily: 'Inter',
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        const SizedBox(width: 16),
-      ],
+      ),
     );
   }
 }
