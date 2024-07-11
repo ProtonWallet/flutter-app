0b9c64954412f851f7b7864d427930b0a7a73b0e
Will Hsu <will.hsu@proton.ch>
2024-07-11 23:37:34 +0800

    show stacktrace for debug

--- lib/constants/address.key.dart
+++ lib/constants/address.key.dart
@@ -4,14 +4,18 @@ import 'dart:typed_data';
 import 'package:proton_crypto/proton_crypto.dart' as proton_crypto;
 import 'package:wallet/helper/extension/data.dart';
 import 'package:wallet/helper/extension/strings.dart';
+import 'package:wallet/helper/logger.dart';
 
 class AddressKey {
   final String id;
   final String privateKey;
   final String passphrase;
 
-  AddressKey(
-      {required this.id, required this.privateKey, required this.passphrase});
+  AddressKey({
+    required this.id,
+    required this.privateKey,
+    required this.passphrase,
+  });
 
   String decryptBinary(String? binaryEncryptedString) {
     if (binaryEncryptedString != null) {
@@ -22,7 +26,10 @@ class AddressKey {
         if (decryptedMessage != "null") {
           return decryptedMessage;
         }
-      } catch (e) {
+      } catch (e, stacktrace) {
+        logger.i(
+          "getHistoryTransactions error: ${e.toString()} stacktrace: ${stacktrace.toString()}",
+        );
         return "";
       }
     }
@@ -32,7 +39,10 @@ class AddressKey {
   String decrypt(String encryptedArmor) {
     try {
       return proton_crypto.decrypt(privateKey, passphrase, encryptedArmor);
-    } catch (e) {
+    } catch (e, stacktrace) {
+      logger.i(
+        "getHistoryTransactions error: ${e.toString()} stacktrace: ${stacktrace.toString()}",
+      );
       return "";
     }
   }
@@ -40,7 +50,10 @@ class AddressKey {
   String encrypt(String plainText) {
     try {
       return proton_crypto.encrypt(privateKey, plainText);
-    } catch (e) {
+    } catch (e, stacktrace) {
+      logger.i(
+        "getHistoryTransactions error: ${e.toString()} stacktrace: ${stacktrace.toString()}",
+      );
       rethrow;
     }
   }
@@ -48,7 +61,10 @@ class AddressKey {
   String encryptBinary(Uint8List data) {
     try {
       return proton_crypto.encryptBinary(privateKey, data).base64encode();
-    } catch (e) {
+    } catch (e, stacktrace) {
+      logger.i(
+        "getHistoryTransactions error: ${e.toString()} stacktrace: ${stacktrace.toString()}",
+      );
       rethrow;
     }
   }
