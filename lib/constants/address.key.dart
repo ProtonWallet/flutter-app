45837fbc3c7c724f334c5d022537790e166006cd
Will Hsu <will.hsu@proton.ch>
2024-07-11 23:30:35 +0800

    fix return error when use gopenpgp_plugin decrypt, layout fix

--- lib/constants/address.key.dart
+++ lib/constants/address.key.dart
@@ -10,29 +10,46 @@ class AddressKey {
   final String privateKey;
   final String passphrase;
 
-  AddressKey({required this.id, required this.privateKey, required this.passphrase});
+  AddressKey(
+      {required this.id, required this.privateKey, required this.passphrase});
 
   String decryptBinary(String? binaryEncryptedString) {
     if (binaryEncryptedString != null) {
-      Uint8List bytes = proton_crypto.decryptBinary(
-          privateKey, passphrase, binaryEncryptedString.base64decode());
-      String? decryptedMessage = utf8.decode(bytes);
-      if (decryptedMessage != "null") {
-        return decryptedMessage;
+      try {
+        Uint8List bytes = proton_crypto.decryptBinary(
+            privateKey, passphrase, binaryEncryptedString.base64decode());
+        String? decryptedMessage = utf8.decode(bytes);
+        if (decryptedMessage != "null") {
+          return decryptedMessage;
+        }
+      } catch (e) {
+        return "";
       }
     }
     return "";
   }
 
   String decrypt(String encryptedArmor) {
-    return proton_crypto.decrypt(privateKey, passphrase, encryptedArmor);
+    try {
+      return proton_crypto.decrypt(privateKey, passphrase, encryptedArmor);
+    } catch (e) {
+      return "";
+    }
   }
 
   String encrypt(String plainText) {
-    return proton_crypto.encrypt(privateKey, plainText);
+    try {
+      return proton_crypto.encrypt(privateKey, plainText);
+    } catch (e) {
+      rethrow;
+    }
   }
 
   String encryptBinary(Uint8List data) {
-    return proton_crypto.encryptBinary(privateKey, data).base64encode();
+    try {
+      return proton_crypto.encryptBinary(privateKey, data).base64encode();
+    } catch (e) {
+      rethrow;
+    }
   }
 }
