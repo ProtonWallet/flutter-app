bb1a2e0fd87f53ca8cc13e81f87b24201f254862
yzhang <yanfeng.zhang@proton.ch>
2024-08-09 17:59:54 +0800

    migrate code to latest dart/flutter

--- lib/scenes/home/navigation.view.dart
+++ lib/scenes/home/navigation.view.dart
@@ -1,7 +1,4 @@
-import 'package:easy_sidemenu/easy_sidemenu.dart';
 import 'package:flutter/material.dart';
-import 'package:wallet/constants/assets.gen.dart';
-import 'package:wallet/helper/logger.dart';
 import 'package:wallet/scenes/core/view.dart';
 import 'package:wallet/scenes/home/navigation.viewmodel.dart';
 
@@ -19,20 +16,6 @@ class HomeNavigationView extends ViewBase<HomeNavigationViewModel> {
     return Scaffold(
       body: Row(
         children: [
-          SideMenu(
-            // Page controller to manage a PageView
-            controller: viewModel.sideMenu,
-            // Will shows on top of all items, it can be a logo or a Title text
-            title: Assets.images.wallet.image(),
-            // Will show on bottom of SideMenu when displayMode was SideMenuDisplayMode.open
-            footer: const Text('demo'),
-            // Notify when display mode changed
-            onDisplayModeChanged: (mode) {
-              logger.d(mode);
-            },
-            // List of SideMenuItem to show them on SideMenu
-            items: viewModel.items,
-          ),
           Expanded(
             child: PageView(
               controller: viewModel.pageController,
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import cryptography_flutter
 import device_info_plus
 import flutter_local_notifications
 import flutter_secure_storage_macos
+import local_auth_darwin
 import package_info_plus
 import path_provider_foundation
 import share_plus
@@ -24,6 +25,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
