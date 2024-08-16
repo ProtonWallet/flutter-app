98b65b92274a7a2314d3ffe55dd980fc49b17cb0
Will Hsu <will.hsu@proton.ch>
2024-08-16 12:29:02 +0800

    add inter font assets

--- test/golden/golden_test.dart
+++ test/golden/golden_test.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:wallet/constants/fonts.gen.dart';
 import 'package:wallet/scenes/components/button.v6.dart';
 
 void main() {
@@ -7,25 +9,10 @@ void main() {
     await loadAppFonts();
   });
 
-  testGoldens('MyWidget golden test', (WidgetTester tester) async {
-    await tester.pumpWidget(const ButtonV6(
-      text: 'Sunny',
-      width: 600,
-      height: 100,
-      isLoading: false,
-    ));
-
-    // Capture the golden image.
-    await expectLater(
-      find.byType(ButtonV6),
-      matchesGoldenFile('goldens/ButtonV6.png'),
-    );
-  }, tags: ['golden']);
-
   testGoldens('Weather types should look correct', skip: false, (tester) async {
     final builder = GoldenBuilder.grid(columns: 1, widthToHeightRatio: 1)
       ..addScenario(
-          'Sunny',
+          'Sunny1',
           const ButtonV6(
             text: 'Sunny',
             width: 300,
@@ -60,7 +47,14 @@ void main() {
             width: 300,
             height: 60,
           ));
-    await tester.pumpWidgetBuilder(builder.build());
+    await tester.pumpWidgetBuilder(
+      builder.build(),
+      wrapper: materialAppWrapper(
+        theme: ThemeData(
+          fontFamily: FontFamily.inter,
+        ),
+      ),
+    );
     await screenMatchesGolden(tester, 'buttonv6_types_grid');
   }, tags: ['golden']);
 }
