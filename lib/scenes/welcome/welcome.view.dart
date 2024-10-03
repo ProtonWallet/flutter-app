d3c431c4dc42fbaac6d6e2e40a2a2ac1e2aaf7a0
Will Hsu <will.hsu@proton.ch>
2024-10-03 17:29:47 +0800

    improve welcome page when native login success

--- lib/scenes/welcome/welcome.view.dart
+++ lib/scenes/welcome/welcome.view.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:wallet/constants/proton.color.dart';
+import 'package:wallet/l10n/generated/locale.dart';
+import 'package:wallet/scenes/components/custom.loading.dart';
 import 'package:wallet/scenes/core/responsive.dart';
 import 'package:wallet/scenes/core/view.dart';
 import 'package:wallet/scenes/core/view.navigatior.identifiers.dart';
@@ -8,6 +11,7 @@ import 'package:wallet/scenes/welcome/login.signup.button.dart';
 import 'package:wallet/scenes/welcome/welcom.backgroud.dart';
 import 'package:wallet/scenes/welcome/welcome.image.dart';
 import 'package:wallet/scenes/welcome/welcome.viewmodel.dart';
+import 'package:wallet/theme/theme.font.dart';
 
 class WelcomeView extends ViewBase<WelcomeViewModel> {
   const WelcomeView(WelcomeViewModel viewModel)
@@ -43,18 +47,20 @@ class WelcomeView extends ViewBase<WelcomeViewModel> {
                       height: max(
                           (MediaQuery.of(context).size.height - 300) / 2, 0),
                     ),
-                    SizedBox(
-                      height: 120,
-                      width: 450,
-                      child: LoginAndSignupBtn(
-                        signupPressed: () {
-                          viewModel.move(NavID.nativeSignup);
-                        },
-                        signinPressed: () {
-                          viewModel.move(NavID.nativeSignin);
-                        },
-                      ),
-                    ),
+                    viewModel.isLoginToHomepage
+                        ? nowLogin(context)
+                        : SizedBox(
+                            height: 120,
+                            width: 450,
+                            child: LoginAndSignupBtn(
+                              signupPressed: () {
+                                viewModel.move(NavID.nativeSignup);
+                              },
+                              signinPressed: () {
+                                viewModel.move(NavID.nativeSignin);
+                              },
+                            ),
+                          ),
                   ],
                 ),
               ),
@@ -68,19 +74,36 @@ class WelcomeView extends ViewBase<WelcomeViewModel> {
               const WelcomeImage(),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: LoginAndSignupBtn(
-                  signupPressed: () {
-                    viewModel.move(NavID.nativeSignup);
-                  },
-                  signinPressed: () {
-                    viewModel.move(NavID.nativeSignin);
-                  },
-                ),
+                child: viewModel.isLoginToHomepage
+                    ? nowLogin(context)
+                    : LoginAndSignupBtn(
+                        signupPressed: () {
+                          viewModel.move(NavID.nativeSignup);
+                        },
+                        signinPressed: () {
+                          viewModel.move(NavID.nativeSignin);
+                        },
+                      ),
               ),
             ],
           ),
         ),
       ),
     ));
+  }
+
+  Widget nowLogin(BuildContext context) {
+    return Column(children: [
+      Text(
+        S.of(context).now_login,
+        style: FontManager.body1Median(ProtonColors.textNorm),
+      ),
+      const SizedBox(
+        height: 8,
+      ),
+      const CustomLoading(
+        size: 26,
+      ),
+    ]);
   }
 }
